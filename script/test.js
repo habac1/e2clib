6269d34656b2f34cc2e171b452acaa83f5394fa3
habac1 (remplacer par pseudo utilisé pour le compte gitHub) <janos.mat7@gmail.com (remplacer par adresse mail utilisz pour le compte gitHub)>
2025-12-01 16:39:23 +0100

    update1

---
+++ script/test.js
@@ -0,0 +1,30 @@
+/*
+let username = "habac1" 
+let number = 25 //integrer
+let double = 12.59 //double
+let boolean = true // ou false 
+
+console.log(username, number, double, boolean);
+
+let number1 = 15
+let number2 = 23
+
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
+console.log(number1 % number2)
+
+username = "geronimo"
+
+console.log(username)
+*/
+let firstname; 
+let lastname;
+
+firstname = prompt("Quel et votre prénom?");
+lastname = prompt("Quel est votre nom de famille?")
+
+alert('Bonjour '    + firstname + " " +  lastname)
+
+alert(`BONJOUR ${firstname} ${lastname}, comment tu-vas?`)
