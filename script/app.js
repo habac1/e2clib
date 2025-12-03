a0922456dc60ccab9ccaae0a6cbf62d7fa70c7fe
habac1 (remplacer par pseudo utilisé pour le compte gitHub) <janos.mat7@gmail.com (remplacer par adresse mail utilisz pour le compte gitHub)>
2025-12-03 11:50:14 +0100

    update3

--- script/app.js
+++ script/app.js
@@ -1,7 +1,13 @@
+
 //definir les variables - récuperer les éléments html dont on a besoin
 const connectButton = document.querySelector("#connect-button")
 const main = document.querySelector("main");
+const connetBox = document.querySelector("#connect-background");
+console.log(connetBox) 
+const closeButton = document.querySelector(".close-button");
+const formBox = document.querySelector("#connet-form")
 let count = 0;
+
 //definir les fonctions - les actions possible
 function addText()  {
      count ++;   
@@ -23,5 +29,22 @@ i
     
    
 }
+function openConnect() {
+   connetBox.style.display= "flex"
+   connetBox.style.opacity = "1"
+ } 
+
+function closeConnect() {
+   connetBox.style.display = "none"
+   connetBox.style.display = "1"
+
+function NoClose(event) {
+   event.stopPropagation()
+}    
+}
+
 // mettre en palce les déclencheurs - écouteurs d'évenements 
-connectButton.addEventListener("click", addText)
+connectButton.addEventListener("click", openConnect) 
+closeButton.addEventListener("click",closeConnect)
+connetBox.addEventListener("click", closeConnect)
+formBox.addEventListener("click", noClose)
